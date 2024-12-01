fa5678780bb9bc3111ea24bfca9238a7314a7625
Tim <lovezww2011@gmail.com>
2024-12-01 21:32:31 +0800

    init nextjs

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import { ResizableComponent } from "@/components/resizable";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="h-screen overflow-hidden">
+      
+      <ResizableComponent />
+    </div>
+  );
+};
+
+export default Page;
